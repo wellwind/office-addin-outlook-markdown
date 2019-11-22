1d97889bdb11e822ec54c5524c1b58093fd4332d
wellwind <wellwind@mail2000.com.tw>
2019-11-22 23:16:10 +0800

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/markdown-pane/markdown-pane.module.ts
+++ src/app/markdown-pane/markdown-pane.module.ts
@@ -1,17 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MarkdownPaneRoutingModule } from './markdown-pane-routing.module';
 import { MarkdownPaneComponent } from './markdown-pane.component';
-
-
 
 @NgModule({
   declarations: [MarkdownPaneComponent],
   imports: [
     CommonModule,
     FormsModule,
+    MatButtonModule,
     MarkdownPaneRoutingModule
   ]
 })
-export class MarkdownPaneModule { }
+export class MarkdownPaneModule {}
